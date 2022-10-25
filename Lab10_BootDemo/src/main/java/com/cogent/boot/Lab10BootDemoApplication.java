da32e17a597291652449dade03f0a02d23b59ccb
ngomezr <47580268+ngomezr@users.noreply.github.com>
2022-10-24 21:07:50 -0700

    update

--- Lab10_BootDemo/src/main/java/com/cogent/boot/Lab10BootDemoApplication.java
+++ Lab10_BootDemo/src/main/java/com/cogent/boot/Lab10BootDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication 
 public class Lab10BootDemoApplication {
-
 	public static void main(String[] args) { 
 		SpringApplication.run(Lab10BootDemoApplication.class, args);
 	}
-
 }
